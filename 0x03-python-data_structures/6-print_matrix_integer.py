4ad6b7f7f1b2a8be6ef5f52e5b4595d372933eef
Diego Varela <2806@holbertonschool.com>
2021-05-04 22:48:12 +0000

    Fix task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
-        len_x = len(matrix[0])
-        len_y = (len(matrix))
-        for y in range(len_y):
-            for x in range(len_x):
-                print("{:d}".format(matrix[y][x]), end='')
-                if x != (len_x - 1):
-                    print(" ", end='')
-                else:
-                    print()
-    else:
-        print("faf")
+    len_x = len(matrix[0])
+    len_y = (len(matrix))
+    if len_x == 0:
+        print()
+        return
+    for y in range(len_y):
+        for x in range(len_x):
+            print("{:d}".format(matrix[y][x]), end='')
+            if x != (len_x - 1):
+                print(" ", end='')
+            else:
+                print()
