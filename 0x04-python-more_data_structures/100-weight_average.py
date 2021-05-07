f6d040eec2757346e2be27cb0d4cf5f5a4a115e7
Diego Varela <2806@holbertonschool.com>
2021-05-07 01:10:55 +0000

    Fix task 100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,9 +2,10 @@
 def weight_average(my_list=[]):
     div = 0
     res = 0
-    if my_list is None:
+    if my_list:
+        for i in my_list:
+            res += (i[0] * i[1])
+            div += i[1]
+        return (res / div)
+    else:
         return (0)
-    for i in my_list:
-        res += (i[0] * i[1])
-        div += i[1]
-    return (res / div)
