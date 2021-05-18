9676ebb201df3aaa3c222740b053373dbfa8dfa8
Diego Varela <2806@holbertonschool.com>
2021-05-18 00:44:44 +0000

    Upload task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    try:
+        for pos in range(x):
+            print("{}".format(my_list[pos]),end='')
+            i += 1
+        print()
+        return i
+    except IndexError:
+        print()
+        return i
