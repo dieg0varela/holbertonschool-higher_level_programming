4cf2555db7120e3b1b85341adc2a72e89f9cab36
Diego Varela <2806@holbertonschool.com>
2021-05-18 00:48:52 +0000

    Upload task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     try:
         for pos in range(x):
-            print("{}".format(my_list[pos]),end='')
+            print("{}".format(my_list[pos]), end='')
             i += 1
     except IndexError:
         print()
