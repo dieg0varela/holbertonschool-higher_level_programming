b51f70e8e8dfc98a1c835db57f4b8d1454645fe0
Diego Varela <2806@holbertonschool.com>
2021-05-18 20:06:32 +0000

    Upload task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Define class Square"""
+
+
+class Square:
+    """Class Square"""
+    def __init__(self, new_size=0):
+        """Init Method load size"""
+        if (isinstance(new_size, int) is False):
+            raise TypeError("size must be an integer")
+        if (new_size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = new_size
+
+    def area(self):
+        """Area calculation"""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """Print square of your size"""
+        if (self.__size == 0):
+            print()
+        else:
+            for x in range(self.__size):
+                for y in range(self.__size):
+                    print("#", end='')
+                print()
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, val):
+        if (isinstance(val, int) is False):
+            raise TypeError("size must be an integer")
+        if (val < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = val
