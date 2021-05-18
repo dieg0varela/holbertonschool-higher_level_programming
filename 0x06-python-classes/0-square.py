91d5dc8b70b5989bf3c361d0169c1ed4202e2596
Diego Varela <2806@holbertonschool.com>
2021-05-18 15:44:02 +0000

    Upload task 1 and fixes 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 class Square:
+    """Empty class Square"""
     pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Square:
+    """Class Square"""
+    def __init__(self, new_size=None):
+        """Init Method load size"""
+        self.__size = new_size
