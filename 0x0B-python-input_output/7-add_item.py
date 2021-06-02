f38caca7539b0b66adb9bda092b62a3ae523ac43
Diego Varela <2806@holbertonschool.com>
2021-06-02 23:10:03 +0000

    Upload task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Load add and save Module'''
+
+
+import json
+from sys import argv
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+size = len(argv) - 1
+lista = []
+try:
+    print("22")
+    lista = load_from_json_file("add_item.json")
+    print(lista)
+except:
+    print("33")
+    lista = []
+for i in range(size):
+    lista.append(argv[i + 1])
+save_to_json_file(lista, "add_item.json")
