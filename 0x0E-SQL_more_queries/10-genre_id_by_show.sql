59a5bd3c2591b17aebe81d0f5a735f2ff884407d
Diego Varela <2806@holbertonschool.com>
2021-07-13 18:55:18 +0000

    Upload task9-10

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,2 @@
+-- List all tv shows with the genres id.
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id ORDER BY tv_shows.title, tv_show_genres.genre_id;
