020bb2c8741ffe855b8bc4d3e8a65d8653672a92
Diego Varela <2806@holbertonschool.com>
2021-08-02 23:59:33 +0000

    Upload task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,8 @@ if (isNaN(argv[2]) || argv[2] === 1) {
   console.log('0');
 }
 
-const arr = argv.slice(2);
-arr.sort();
-const len = arr.length;
-console.log(arr[len - 2]);
+let arr = argv.slice(2);
+let cosa = arr.map(x => parseInt(x));
+cosa.sort();
+const len = cosa.length;
+console.log(cosa[len - 2]);
