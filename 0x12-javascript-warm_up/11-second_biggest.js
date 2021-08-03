d9f25d0e7fcd8b9a240df94f6a224febb98f129e
Diego Varela <2806@holbertonschool.com>
2021-08-03 00:15:51 +0000

    Fix task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const { argv } = require('process');
 
-if (isNaN(argv[2]) || argv[2] === 1) {
+if (isNaN(argv[2]) || argv[2] === '1') {
   console.log('0');
+} else {
+  const arr = argv.slice(2);
+  const cosa = arr.map(x => parseInt(x));
+  cosa.sort(function (a, b) { return a - b; });
+  const len = cosa.length;
+  console.log(cosa[len - 2]);
 }
-
-const arr = argv.slice(2);
-const cosa = arr.map(x => parseInt(x));
-cosa.sort(function (a, b) { return a - b; });
-const len = cosa.length;
-console.log(cosa[len - 2]);
