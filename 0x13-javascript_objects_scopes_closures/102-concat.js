7d91405659e71f1cbdc62a90a3dc42701946e833
Diego Varela <2806@holbertonschool.com>
2021-08-04 01:55:25 +0000

    Upload task 100 and 102

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const fs = require("fs");
+const { argv } = require('process');
+
+fs.readFile(argv[2], function (err, data) {
+  if (err) throw err;
+  fs.writeFile(argv[4], data, function(err) {
+   if (err) {
+      return console.error(err);
+   }
+  });
+});
+
+fs.readFile(argv[3], function (err, data) {
+  if (err) throw err;
+  fs.appendFile(argv[4], data, function (err) {
+    if (err) throw err;
+  });
+});
+
