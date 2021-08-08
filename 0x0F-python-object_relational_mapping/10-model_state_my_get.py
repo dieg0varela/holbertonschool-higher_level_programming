bccfcce8735a8fa94e848641f0cf581043de0878
Diego Varela <2806@holbertonschool.com>
2021-08-08 21:11:28 +0000

    Upload task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''List all States using SQLAlchemy'''
+
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    instance = (session.query(State).filter(text("name=('{}')"
+                     .format(sys.argv[4]))).order_by(State.id))
+
+    if instance.count() != 0:
+        print(str(instance.id))
+    else:
+        print("Not Found")
