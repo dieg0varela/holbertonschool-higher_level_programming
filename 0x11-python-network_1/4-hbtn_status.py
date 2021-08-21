94100b2f2e78e0c23d975d7d61618d0e16dc338f
root <2806@holbertonschool.com>
2021-08-21 02:26:16 +0000

    Fix task 5

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 '''Task 4 Module'''
 import requests
-response = requests.get('https://intranet.hbtn.io/status')
-html = response.text
-print("Body response:")
-print("	- type: {}".format(type(html)))
-print("	- content: {}".format(html))
+if __name__ == "__main:__"
+    response = requests.get('https://intranet.hbtn.io/status')
+    html = response.text
+    print("Body response:")
+    print("	- type: {}".format(type(html)))
+    print("	- content: {}".format(html))
