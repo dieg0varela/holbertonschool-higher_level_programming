823114f243b15fb47bab5c45f2d4e5ad636b5fb6
Diego Varela <2806@holbertonschool.com>
2021-09-15 01:55:08 +0000

    Fix task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,23 +5,15 @@ const request = require('request');
 request(argv[2], function (err, res, body) {
   if (err) console.log(err);
   const data = JSON.parse(body);
-  const pichu = {};
-  let id = data[0].userId;
-  let num = 0;
+  const rai = {};
   data.forEach(element => {
-    if (element.userId === id) {
-      if (element.completed === true) {
-        num++;
-      }
-    } else {
-      if (num !== 0) pichu[id] = num;
-      id = element.userId;
-      num = 0;
-      if (element.completed === true) {
-        num++;
+    if (element.completed === true) {
+      if (element.userId in rai) {
+        rai[element.userId] += 1;
+      } else {
+        rai[element.userId] = 1;
       }
     }
   });
-  if (num !== 0) pichu[id] = num;
-  console.log(pichu);
+  console.log(rai);
 });
